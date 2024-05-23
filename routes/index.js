e17d8ec699641c939a1188d50ca2a423872c0f2b
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-05-23 19:13:03 +0200

    Add test redirects after authentication

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,16 @@ import isAuthenticated, { isAdmin } from '../controllers/authenticationControlle
 
 const router = express.Router();
 
-router.get('/', isAuthenticated, isAdmin, (req, res) => {
-    res.send('Hello');
+router.all('/', isAuthenticated, (req, res) => {
+    if (req.session.user.userRole === 'admin') {
+        res.redirect('/dashboard');
+    } else {
+        res.send('Home');
+    }
+})
+
+router.get('/dashboard', isAuthenticated, isAdmin, (req, res) => {
+    res.send('Dashboard');
 })
 
 export default router;
