93353ecae3844a092ecefa834a6196b69b9d49c0
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-06-05 02:20:06 +0200

    Learn cookie session

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
 import express from 'express';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import cookieSession from 'cookie-session';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send("hello");
+// app.use(helmet());
+app.use(cookieParser())
+app.use(cookieSession({
+    name: 'session',
+    keys: ['my-secret-key'],
+    maxAge: 24 * 60 * 60* 1000
+}));
+
+
+app.get('/set-session', (req, res) => {
+    req.session.user = { name: 'john doe' };
+    console.log(req.session.user);
+    if (req.session.user) {
+        res.send('Session cookie has been set');
+    } else {
+        res.send('error');
+    }
 })
+
+app.get('/read-session', (req, res) => {
+    const user = req.session.user;
+    res.json(user || 'No session data');
+})
+
+app.get('/clear-session', (req, res) => {
+    req.session = null;
+    res.send('Session cookie has been cleared');
+})
+
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`App listening on ${port}`);
