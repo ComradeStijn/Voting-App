3508396a6a221d20919ffe2730bb3180628dbb63
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-06-05 23:34:11 +0200

    Add client error message

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,14 +3,17 @@ import { findUserByToken } from "../models/authModel.js";
 const login = (req, res, next) => {
     console.log('login controller invoked');
     const token = req.body.token;
-    console.log(token);
     const user = findUserByToken(token);
     if (user) {
         req.session.user = user;
-        res.redirect('/index');
+        res.status(200).json({ success: true, message: 'Logged in succesfully'});
     } else {
-        res.status(401).json({ message: 'Invalid token' });
+        res.status(401).json({ success: false, message: 'Invalid token' });
     }
 };
+
+const authenticate = (req, res, next) => {
+    
+}
 
 export { login };
