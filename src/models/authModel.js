84ed8bdd428e7c35df5f68f41bf1a72395cc5325
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-06-08 02:18:05 +0200

    Add create user and find user queries

--- src/models/authModel.js
+++ src/models/authModel.js
@@ -1,4 +1,4 @@
-
+import { db } from "../app.js";
 
 const userArray = [
     { id: 1, name: "Stijn Doe", token: "123123123", votes: 3},
@@ -6,10 +6,46 @@ const userArray = [
 ];
 
 
+// export function findUserByToken(token) {
+//     const user =  userArray.find((user) => {
+//         return user.token === token;
+//     });
+//     return user;
+// }
+
+
 export function findUserByToken(token) {
-    const user =  userArray.find((user) => {
-        return user.token === token;
-    });
-    return user;
+    const findQuery = 'SELECT * FROM users WHERE token = ?';
+    const transformedToken = token.toString();
+    try {
+        const user = db.prepare(findQuery).get(transformedToken);
+        return;
+    } catch (err) {
+        console.error(`Error ${err.code}: ${err.message}`);
+    }
 }
+
+
+
+
+export function findAllUsers() {
+    const selectQuery = 'SELECT * FROM users';
+    try {
+        const users = db.prepare(selectQuery).all();
+        return users;
+    } catch (err) {
+        console.error(`Error ${err.code}: ${err.message}`);
+    }
+}
+
+export function createUser(name, token, votes) {
+    const insertQuery = 'INSERT INTO users (name, token, votes) VALUES (?,?,?)';
+    try {
+        return db.prepare(insertQuery).run(name, token, votes);
+    } catch (err) {
+        console.error(`Error ${err.code}: ${err.message}`);
+    }
+}
+
+
 
